22d53de4424f942c8bc38769ce144e0b6d4b2f6c
gabrielgws <gabrielgws.dev@gmail.com>
2023-07-12 09:52:51 -0300

    feat: Dados no template

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./first-component.component.scss']
 })
 export class FirstComponentComponent {
+  name: string = 'Gabriel';
+  age: number = 29;
+  job = 'Dev';
+  hobbies = ["Correr", "Jogar", "Estudar"];
+  car = {
+    name: "Gol",
+    year: 2022
+  }
 
 }
