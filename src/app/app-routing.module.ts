0d440c27655410a8b551b717a4ace5fbdfaf1536
gabrielgws <gabrielgws.dev@gmail.com>
2023-07-19 09:10:16 -0300

    feat: Angular Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { EventosComponent } from './components/eventos/eventos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FirstComponentComponent },
+  {path: 'list', component: ListRenderComponent },
+  {path: 'event', component: EventosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
