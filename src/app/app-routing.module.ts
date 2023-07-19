65165ea0c074b634ef04dcc48efb0ffae0c17fb7
gabrielgws <gabrielgws.dev@gmail.com>
2023-07-19 09:49:25 -0300

    feat: Dynamic routes - selecionando dado individual

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
-import { EventosComponent } from './components/eventos/eventos.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 
 const routes: Routes = [
   {path: '', component: FirstComponentComponent },
   {path: 'list', component: ListRenderComponent },
-  {path: 'event', component: EventosComponent },
+  {path: 'list/:id', component: ItemDetailComponent },
 ];
 
 @NgModule({
