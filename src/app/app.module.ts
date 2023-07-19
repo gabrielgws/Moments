75a2049d690b46b32c438405ae17f8b82682af69
gabrielgws <gabrielgws.dev@gmail.com>
2023-07-19 09:32:37 -0300

    feat: Requisições HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,6 +33,7 @@ import { TwoWayBindingComponent } from './components/two-way-binding/two-way-bin
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],
--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -11,16 +11,13 @@ import { ListService } from 'src/app/services/list.service';
 })
 
 export class ListRenderComponent {
-  animals: Animal[] = [
-    {name: "Turca", type: "Dog", age: 4},
-    {name: "Tom", type: "Cat", age: 10},
-    {name: "Frida", type: "Dog", age: 5},
-    {name: "Bob", type: "Horse", age: 1},
-  ]
+  animals: Animal[] = [];
 
   animalDetails = '';
 
-  constructor(private listService: ListService) {}
+  constructor(private listService: ListService) {
+    this.getAnimals();
+  }
 
   showAge(animal: Animal):void {
     this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`
@@ -29,5 +26,9 @@ export class ListRenderComponent {
   removeAnimal(animal: Animal) {
     console.log("removendo animal..");
     this.animals = this.listService.remove(this.animals, animal);
+  }
+
+  getAnimals(): void {
+    this.listService.getAll().subscribe((animals) => (this.animals = animals));
   }
 }
