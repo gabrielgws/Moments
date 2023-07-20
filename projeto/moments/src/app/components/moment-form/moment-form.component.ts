7598c9c793c0645bfe7b6fa0e32bd0863f4cd59f
gabrielgws <gabrielgws.dev@gmail.com>
2023-07-20 10:59:12 -0300

    feat: Validação de formulário com Reactive Forms

--- projeto/moments/src/app/components/moment-form/moment-form.component.ts
+++ projeto/moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
@@ -6,6 +7,33 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./moment-form.component.scss']
 })
 export class MomentFormComponent {
-  @Input() btnText!: string
+  @Input() btnText!: string;
+
+  momentForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    });
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    if (this.momentForm.invalid) {
+      return;
+    }
+
+    console.log("Enviou o formulário!")
+  }
 
 }
