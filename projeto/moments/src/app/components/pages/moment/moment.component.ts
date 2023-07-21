9d52b7a785e803facaf361e0a8cf44a3b6383d92
gabrielgws <gabrielgws.dev@gmail.com>
2023-07-21 11:29:05 -0300

    feat: Página individual de momento

---
+++ projeto/moments/src/app/components/pages/moment/moment.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { MomentService } from 'src/app/services/moment.service';
+import { Moment } from 'src/app/Moment';
+
+@Component({
+  selector: 'app-moment',
+  templateUrl: './moment.component.html',
+  styleUrls: ['./moment.component.scss']
+})
+export class MomentComponent {
+  moment?: Moment;
+
+  constructor(
+    private momentService: MomentService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.momentService.getMoment(id).subscribe(item => this.moment = item.data);
+  }
+
+}
